33217e7031213e22e65ba52cacc8169c652c25ad
Mads Felskov Agersten <mfa@appear.dk>
2020-05-13 12:26:01 +0200

    feat: add delayed hello messages

--- packages/pkg2/lib/index.js
+++ packages/pkg2/lib/index.js
@@ -2,4 +2,8 @@
 
 module.exports = (...params) => {
   console.log("Hello from test package 2", ...params);
+
+  setTimeout(() => {
+    console.log("Delayed hello from package 2");
+  }, 1000);
 };
